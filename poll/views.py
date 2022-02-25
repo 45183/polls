9063b881589e19b79e43736a7b09ac505edbaa15
45183 <hsb9743@naver.com>
2022-02-25 12:47:39 +0900

    day38

--- poll/views.py
+++ poll/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from poll.models import Question
 
 # Create your views here.
 def index(request):
-    return render(request, 'poll/index.html')
+    question_list = Question.objects.all()
+    return render(request, 'poll/index.html', {'question_list':question_list})
     # return HttpResponse("<h1>안녕~ Django!!</h1>")
 
-def cart(request):
-    cart = "계란"
-    cartlist = ["계란", "두부", "커피"]
-    return render(
-        request,
-        'poll/cart.html',
-        {'cart':cart,'cartlist':cartlist}
-    )
+def detail(request, pk):
+    question = Question.objects.get(id=pk)              # 자료 1개 가져오기
+    return render(request, 'poll/detail.html', {'question':question})
+
+def vote(request, pk):
+    pass
+
+# def cart(request):
+#     cart = "계란"
+#     cartlist = ["계란", "두부", "커피"]
+#     return render(
+#         request,
+#         'poll/cart.html',
+#         {'cart':cart,'cartlist':cartlist}
+#     )
